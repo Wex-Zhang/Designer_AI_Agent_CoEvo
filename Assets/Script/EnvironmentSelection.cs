f7f9cfa7cd81a808d9b15bcb8da0360692bff135
Wex-Zhang <wex6899@gmail.com>
2024-09-09 22:10:13 +0800

    Add environment select page
    version1

---
+++ Assets/Script/EnvironmentSelection.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.EventSystems;
+
+public class EnvironmentSelection : MonoBehaviour
+{
+    public Button button1;
+    public Button button2;
+    public VideoPlayer videoPlayer1;
+    public VideoPlayer videoPlayer2;
+    public GameObject gameObject1;
+    public GameObject gameObject2;
+    public Button nextButton;
+    private Button selectedButton;
+
+    void Start()
+    {
+        // 添加悬停和移出事件
+        AddHoverEvent(button1, videoPlayer1);
+        AddHoverEvent(button2, videoPlayer2);
+
+        // 添加点击事件
+        button1.onClick.AddListener(() => SelectButton(button1, videoPlayer1));
+        button2.onClick.AddListener(() => SelectButton(button2, videoPlayer2));
+
+        // Next按钮事件
+        nextButton.onClick.AddListener(OnNextClicked);
+    }
+
+    void AddHoverEvent(Button button, VideoPlayer videoPlayer)
+    {
+        EventTrigger trigger = button.gameObject.AddComponent<EventTrigger>();
+
+        // 悬停事件：播放视频
+        EventTrigger.Entry entryHover = new EventTrigger.Entry();
+        entryHover.eventID = EventTriggerType.PointerEnter;
+        entryHover.callback.AddListener((eventData) => PlayVideo(videoPlayer));
+        trigger.triggers.Add(entryHover);
+
+        // 鼠标移出事件：停止视频
+        EventTrigger.Entry entryExit = new EventTrigger.Entry();
+        entryExit.eventID = EventTriggerType.PointerExit;
+        entryExit.callback.AddListener((eventData) =>
+        {
+            // 如果当前按钮没有被选中，移出后停止视频
+            if (selectedButton != button)
+            {
+                StopVideo(videoPlayer);
+            }
+        });
+        trigger.triggers.Add(entryExit);
+    }
+
+    void PlayVideo(VideoPlayer videoPlayer)
+    {
+        videoPlayer.Play();
+    }
+
+    void StopVideo(VideoPlayer videoPlayer)
+    {
+        videoPlayer.Stop();
+    }
+
+    void SelectButton(Button button, VideoPlayer videoPlayer)
+    {
+        // 清除之前的白框和视频播放
+        if (selectedButton != null && selectedButton != button)
+        {
+            DeselectButton(selectedButton);
+            StopVideo(selectedButton == button1 ? videoPlayer1 : videoPlayer2);
+        }
+
+        // 选中当前按钮并显示白框
+        selectedButton = button;
+        Outline outline = button.GetComponent<Outline>();
+        if (outline == null)
+        {
+            outline = button.gameObject.AddComponent<Outline>();
+        }
+        outline.effectColor = new Color(1, 1, 1, 0.1f);  // 1,1,1表示白色，0.1f表示10%不透明度
+
+        // 选中的按钮视频继续播放
+        PlayVideo(videoPlayer);
+    }
+
+    void DeselectButton(Button button)
+    {
+        // 移除白框
+        Outline outline = button.GetComponent<Outline>();
+        if (outline != null)
+        {
+            Destroy(outline);
+        }
+    }
+
+    void OnNextClicked()
+    {
+        if (selectedButton == button1)
+        {
+            gameObject1.SetActive(true);
+            gameObject2.SetActive(false);
+        }
+        else if (selectedButton == button2)
+        {
+            gameObject1.SetActive(false);
+            gameObject2.SetActive(true);
+        }
+    }
+}
